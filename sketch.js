46f02b18e3a4a75bd008db5f9273066bf67e1500
1602341 <147089725+1602341@users.noreply.github.com>
2023-11-22 12:17:15 -0600

    started following tutorial to flood fil

--- sketch.js
+++ sketch.js
@@ -13,29 +13,86 @@
 // Pixels:
 // https://p5js.org/reference/#/p5/pixels
 
-let section;
+
+// let x;
+// let y; 
+// let section;
+let grid;
+let cellSize;
+const GRID_SIZE = 50;
 
 function setup() {
   createCanvas(600, 600);
-  loadPixels();
-  let d = pixelDensity();
+  //loadPixels();
+  grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  if (height >= width) {
+    cellSize = width/GRID_SIZE
+  }
+  else if (height < width) {
+    cellSize = height/GRID_SIZE
+  }
+  //let d = pixelDensity();
 }
+
 
 function draw() {
   background(220);
-  for (let y = 0; y < height; y++) {
-    for (let x = 0; x < width; x++) {
-      noStroke();
-      circle(mouseX, mouseY, 100);
+  displayGrid();
+  //drawShape();
+  //circle(width/2, height/2, 200);
+}
+
+// function setGrid() {
+//   for (let i = 0; i < GRID_SIZE; i++) {
+//     grid[i] = [];
+//     for (let j = 0; j < GRID_SIZE; j++) {
+//       grid[i][j] = '#FFFFFF';
+//     }
+//   }
+
+//   drawShape();
+// }
+
+function drawShape() {
+  let gap = 5;
+
+  for (let y = gap; y < GRID_SIZE - gap; y++) {
+    grid[y][gap];
+    grid[gap][y];
+    grid[GRID_SIZE - (gap + 1)][y];
+    grid[i][GRID_SIZE - (gap + 1)];
+  }
+}
+
+function displayGrid() {
+  for (let y = 0; y < GRID_SIZE; y++) { 
+    for (let x = 0; x < GRID_SIZE; x++) {
+      fill("white");
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
     }
   }
 }
 
-function fillFinder() {
-  let section = 4 * (d * width) * (d * height / 2);
+function generateEmptyGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+        newGrid[y].push(0);
+    }
+  }
+  drawShape();
+  return newGrid;
 }
+// function validPixel(x, y, color) {
+//   return x > 0 && x < width && y > 0 && y < height && 
+// }
 
-function dfs(section, x, y, oldColor, newColor) {
+// function fillFinder() {
+//   let section = 4 * (d * width) * (d * height / 2);
+// }
 
-}
+// function dfs(section, x, y, oldColor, newColor) {
+
+// }
 
