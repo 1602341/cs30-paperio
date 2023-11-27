2da9344dc257c20a7e6e5e0db098e0db215776a3
1602341 <147089725+1602341@users.noreply.github.com>
2023-11-27 12:15:32 -0600

    trying to implement flood fill

--- sketch.js
+++ sketch.js
@@ -21,18 +21,22 @@
 let grid;
 let gap;
 let cellSize;
+let state = "blank";
+let x;
+let y;
+let newColor;
 const GRID_SIZE = 50;
 
 function setup() {
   createCanvas(600, 600);
-  //loadPixels();
-  //grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
-  // if (height >= width) {
-  //   cellSize = width/GRID_SIZE
-  // }
-  // else if (height < width) {
-  //   cellSize = height/GRID_SIZE
-  // }
+  loadPixels();
+  grid = generateEmptyGrid(GRID_SIZE, GRID_SIZE);
+  if (height >= width) {
+    cellSize = width/GRID_SIZE
+  }
+  else if (height < width) {
+    cellSize = height/GRID_SIZE
+  }
   pixelDensity(5);
 }
 
@@ -41,62 +45,80 @@ function setup() {
 function draw() {
   background(220);
   circle(width/2, height/2, 200);
-  //displayGrid();
+  displayGrid();
   //drawShape();
   //circle(width/2, height/2, 200);
 }
 
 function mousePressed() {
-  loadPixels();
-  let r = pixels[(mouseY * width + mouseX) * 4];
-  let g = pixels[(mouseY * width + mouseX) * 4 + 1];
-  let b = pixels[(mouseY * width + mouseX) * 4 + 2];
-  bucket()//mouseX, mouseY, 0, random(255), random(255), random(255), r, g, b);
-  updatePixels();
+  floodFill();
+  // loadPixels();
+  // let r = pixels[(mouseY * width + mouseX) * 4];
+  // let g = pixels[(mouseY * width + mouseX) * 4 + 1];
+  // let b = pixels[(mouseY * width + mouseX) * 4 + 2];
+  // bucket(mouseX, mouseY, 0, random(255), random(255), random(255), r, g, b);
+  // updatePixels();
 }
 
-function bucket() {//x, y, ii, R, G, B, ro, go, bo) {
-  // let r = pixels[(y * width + x) * 4];
-  // let g = pixels[(y * width + x) * 4 + 1];
-  // let b = pixels[(y * width + x) * 4 + 2];
-  // if (r === ro && g === go && b === bo) {
-  //   pixels[(y * width + x) * 4] = R;
-  //   pixels[(y * width + x) * 4+1] = G;
-  //   pixels[(y * width + x) * 4+2] = B;
-  //   for (let i = -1; i <= 1; i++) {
-  //     if (i === 0) continue;
-  //     if (x + i >= width) break;
-  //     if (x + i < 0) break;
-  //     bucket(x + i, y, ii+1, R, G, B, ro, go, bo);
-  //   }
-  //   for (let i = -1; i <= 1; i++) {
-  //     if (i === 0) continue;
-  //     if (y + i >= width) break;
-  //     if (y + i < 0) break;
-  //     bucket(x, y + i, ii+1, R, G, B, ro, go, bo);
-  //   }
-  // }
-  fill("red")
+function floodFill(x, y, newColor) {
+  for (let y = 0; y < GRID_SIZE; y++) { 
+    for (let x = 0; x < GRID_SIZE; x++) {
+      if (x <= 0 || x > width || y <= 0 || y > height || state !== "blank") {
+        return;
+      }
+      else {
+        newColor = "red"
+        grid[y][x] = "full"
+        floodFill(x + 1, y, newColor);
+        floodFill(x - 1, y, newColor);
+        floodFill(x, y + 1, newColor);
+        floodFill(x, y - 1, newColor);
+      }
+    }
+  }
 }
 
-
-
-// function displayGrid() {
-//   for (let y = 0; y < GRID_SIZE; y++) { 
-//     for (let x = 0; x < GRID_SIZE; x++) {
-//       fill("white");
-//       rect(x * cellSize, y * cellSize, cellSize, cellSize);
+// function bucket(x, y, ii, R, G, B, ro, go, bo) {
+//   let r = pixels[(y * width + x) * 4];
+//   let g = pixels[(y * width + x) * 4 + 1];
+//   let b = pixels[(y * width + x) * 4 + 2];
+//   if (r === ro && g === go && b === bo) {
+//     pixels[(y * width + x) * 4] = R;
+//     pixels[(y * width + x) * 4+1] = G;
+//     pixels[(y * width + x) * 4+2] = B;
+//     for (let i = -1; i <= 1; i++) {
+//       if (i === 0) continue;
+//       if (x + i >= width) break;
+//       if (x + i < 0) break;
+//       bucket(x + i, y, ii+1, R, G, B, ro, go, bo);
+//     }
+//     for (let i = -1; i <= 1; i++) {
+//       if (i === 0) continue;
+//       if (y + i >= width) break;
+//       if (y + i < 0) break;
+//       bucket(x, y + i, ii+1, R, G, B, ro, go, bo);
 //     }
 //   }
 // }
 
-// function generateEmptyGrid(cols, rows) {
-//   let newGrid = [];
-//   for (let y = 0; y < rows; y++) {
-//     newGrid.push([]);
-//     for (let x = 0; x < cols; x++) {
-//         newGrid[y].push(0);
-//     }
-//   }
-//   return newGrid;
-// }
+
+
+function displayGrid() {
+  for (let y = 0; y < GRID_SIZE; y++) { 
+    for (let x = 0; x < GRID_SIZE; x++) {
+      fill("white");
+      rect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
+}
+
+function generateEmptyGrid(cols, rows) {
+  let newGrid = [];
+  for (let y = 0; y < rows; y++) {
+    newGrid.push([]);
+    for (let x = 0; x < cols; x++) {
+        newGrid[y].push(0);
+    }
+  }
+  return newGrid;
+}
